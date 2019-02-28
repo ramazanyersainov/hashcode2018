ff0308e6d0f8296770f9f4083c89e55a9655d36e
LuttaMustache <olzhas.290100@gmail.com>
2019-03-01 00:28:07 +0600

    com1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class photo:
     def __init__(self, i, or, tags):
+    def __init__(self, i, orien, tags):
         self.index = i
         self.tags = set(tags)
         self.or = or
+        self.orien = orien
 
 class slide:
     def __init__(self, p_list):
@@ -23,11 +25,21 @@ class slide:
 
 def score(sl):
     if sl.index == N
+    if sl.index == len(sl_list) - 1:
+        return 0;
     set1 = set(sl.tags)
     set2 = set(sl_list[sl.index + 1].tags)
+    return min(len(set1 & set2), len(set1 - set2), len(set2 - set1))
+
 
 if __name__ == "__main__":
     pass
     #Comment
     photo_list = []
     file = open("")
+    p1 = photo(0, 'H', ['3', '3'])
+    p2 = photo(1, 'V', ['3', '3'])
+    s1 = slide([p1])
+    s2 = slide([p2])
+    sl_list = [s1, s2]
+    print(score(p1))
