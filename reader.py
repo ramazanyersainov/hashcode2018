cf1bb12f2cc33d4bacc8989e4b63bd5e2c90a0ef
ramazanyersainov <ramazan.yersainov@nu.edu.kz>
2019-03-01 01:31:46 +0600

    poodele22

--- reader.py
+++ reader.py
@@ -5,7 +5,7 @@ class photo:
         self.orien = orien
     def __repr__(self):
         return "index = {}\t orien = {}\t tags = {}\n".format(self.index,self.orien,self.tags)
-
+'''
 class slide:
     def __init__(self, p_list):
         if len(p_list) > 2 or len(p_list) < 1:
@@ -15,23 +15,69 @@ class slide:
             if  not (p_list[0] == 'V' and p_list[1] == 'V'):
                 print("not two verticals for slide")
                 return
-        self.tags = set()
-        self.p_i_list=[]
+        self.tags = set(p_list[0].tags)
+        self.occup = 0
+        self.p_i_list=[p_list[0].index]
+        
+'''
 
+class slide:
+    def __init__(self, p_list):
+        if len(p_list) > 2 or len(p_list) < 1:
+            print("wrong number of photos for slide")
+            return
+        if len(p_list) == 2:
+            if p_list[1].orien != 'V' or p_list[1].orien != 'V':
+                print("not two verticals for slide")
+                return
+        self.tags = set()
+        self.occup = 0
+        self.p_i_list=[]
         for p in p_list:
+            self.p_i_list.append(p.index)
             self.tags |= p.tags
 
-
+    def __repr__(self):
+        return '{}\t{}'.format(self.tags , self.p_i_list)
 
 def score(sl):
-    if sl.index == N:
-        set1 = set(sl.tags)
-        set2 = set(sl_list[sl.index + 1].tags)
+
+    if sl.index == len(sl_list):
+        return 0
+
+    set1 = set(sl.tags)
+    set2 = set(sl_list[sl.index + 1].tags)
+    return min(len(set1 & set2), len(set1 - set2), len(set2 - set1))
+
+def make_sl_list(p_list):
+    res = []
+    p_V_list = []
+    for p in p_list:
+        if p.orien == 'H':
+            res.append(slide([p]))
+        else:
+            p_V_list.append(p)
+    p_taken = [False for i in range(len(p_V_list))]
+    for i in range(len(p_V_list)):
+        max = 0
+        if p_taken[i]:
+            continue
+        p_max = i
+        for j in range(len(p_V_list)):
+            if (i != j and p_taken[i] == False and p_taken[j] == False):
+                if (len(p_V_list[i].tags | p_V_list[j].tags) >= max):
+                    max = len(p_V_list[i].tags | p_V_list[j].tags)
+                    p_max = j
+        res.append(slide([p_V_list[i], p_V_list[p_max]]))
+        p_taken[i] = True
+        p_taken[p_max] = True
+
+    return res
 
 if __name__ == "__main__":
 
     photo_list = []
-    file_input = open("c_memorable_moments.txt")
+    file_input = open("c_memorable_moments.txt","r")
 
     N = file_input.readline()
     N = int(N)
@@ -46,8 +92,60 @@ if __name__ == "__main__":
                 tag_dict[tag] = [i]
             else:
                 tag_dict[tag].append(i)
-    print(photo_list)
 
-    print(tag_dict)
+    file_input.close()
+    #print(photo_list)
+
+    #print(tag_dict)
+
+    sl_list = make_sl_list(photo_list)
+    print(sl_list)
+
+    output_sl = []
+
+    '''
+    for i in range(0, len(sl_list)):
+        if sl_list[i].occup:
+            continue
+
+        for tag in sl_list[i].tags:
+            index_dict = {}
+            for index in tag_dict[tag]:
+                if index not in index_dict:
+                    index_dict[index]=1
+                else:
+                    index_dict[index]+=1
+
+            max = 0
+            for index in index_dict:
+                if max < index_dict[index]:
+                    max = index
+
+            #print("\n{}\n".format(max))
+
+        sl_list[i].occup = 1
+        sl_list[max].occup = 1
+
+        output_sl.append(sl_list[i])
+        output_sl.append(sl_list[max])
+
+    output_file = open("output.txt","w")
+    output_file.write(str(len(output_sl)))
+    output_file.write("\n")
+
+    for slide in output_sl:
+        for i in range(0,len(slide.p_i_list)):
+            if i != 0:
+                output_file.write(" ")
+            output_file.write(str(slide.p_i_list[i]))
+        output_file.write("\n")
+
+    output_file.close()
+    '''
+
+
+
+
+
 
 
